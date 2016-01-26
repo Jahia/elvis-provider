088203b330d96d108e62059e6e416f46a1725c87
dgaillard <dgaillard@jahia.com>
2016-01-26 18:33:17 +0100

    PRODMARK-232 fix issue with get function due to an error in the header

--- src/main/java/org/jahia/modules/external/elvis/ElvisDataSource.java
+++ src/main/java/org/jahia/modules/external/elvis/ElvisDataSource.java
@@ -1,10 +1,8 @@
 package org.jahia.modules.external.elvis;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -12,7 +10,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.jahia.modules.external.ExternalContentStoreProvider;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +28,6 @@ import java.util.List;
 public class ElvisDataSource extends FilesDataSource {
     private static final Logger logger = LoggerFactory.getLogger(ElvisDataSource.class);
 
-    private String ELVIS_BASE_PATH = "/services";
     private CookieStore cookieStore = new BasicCookieStore();
     private CloseableHttpClient httpClient;
     private ExternalContentStoreProvider externalContentStoreProvider;
@@ -49,7 +45,7 @@ public class ElvisDataSource extends FilesDataSource {
     public List<ExternalFile> getChildrenFiles(String path) throws RepositoryException {
         List<ExternalFile> childrenList = new ArrayList<>();
         try {
-            HttpResponse browseResponse = get("/browse?path=" + path);
+            CloseableHttpResponse browseResponse = get("/browse?path=" + path);
             if (browseResponse.getStatusLine().getStatusCode() == 200) {
                 JSONArray jsonArray = new JSONArray(EntityUtils.toString(browseResponse.getEntity()));
                 for (int i = 0; i < jsonArray.length(); i++) {
@@ -58,21 +54,21 @@ public class ElvisDataSource extends FilesDataSource {
                 }
             }
 
-//            HttpResponse searchResponse = get("/search?q=folderPath:" + path);
-//            if (searchResponse.getStatusLine().getStatusCode() == 200) {
-//                logger.info("HERE HERE HERE");
-//                JSONObject jsonObject = new JSONObject(searchResponse.getEntity().getContent());
-//                if (jsonObject.has("hits")) {
-//                    JSONArray searchJsonArray = new JSONArray(jsonObject.getJSONArray("hits"));
-//                    for (int i = 0 ; i < searchJsonArray.length() ; i++) {
-//                        JSONObject element = searchJsonArray.getJSONObject(i);
-//                        String elPath = element.get("assetPath").toString();
-//                        long created = element.getJSONObject("assetCreated").getLong("value");
-//                        long modified = element.getJSONObject("assetModified").getLong("value");
-//                        childrenList.add(new ExternalFile(ExternalFile.FileType.FILE, elPath, new Date(created), new Date(modified)));
-//                    }
-//                }
-//            }
+            CloseableHttpResponse searchResponse = get("/search?q=folderPath:" + path);
+            if (searchResponse.getStatusLine().getStatusCode() == 200) {
+                JSONObject jsonObject = new JSONObject(EntityUtils.toString(searchResponse.getEntity()));
+                if (jsonObject.has("hits")) {
+                    JSONArray searchJsonArray = jsonObject.getJSONArray("hits");
+                    for (int i = 0 ; i < searchJsonArray.length() ; i++) {
+                        JSONObject element = searchJsonArray.getJSONObject(i);
+                        JSONObject elMetadata = element.getJSONObject("metadata");
+                        String elPath = elMetadata.get("assetPath").toString();
+                        long created = elMetadata.getJSONObject("assetCreated").getLong("value");
+                        long modified = elMetadata.getJSONObject("assetModified").getLong("value");
+                        childrenList.add(new ExternalFile(ExternalFile.FileType.FILE, elPath, new Date(created), new Date(modified)));
+                    }
+                }
+            }
 
             return childrenList;
         } catch (Exception e) {
@@ -105,7 +101,7 @@ public class ElvisDataSource extends FilesDataSource {
     @Override
     public void start() {
         this.context = HttpClientContext.create();
-        this.context.setCookieStore(cookieStore);
+        this.context.setCookieStore(this.cookieStore);
         this.httpClient = HttpClientBuilder.create().setDefaultCookieStore(this.cookieStore).build();
     }
 
@@ -113,10 +109,8 @@ public class ElvisDataSource extends FilesDataSource {
     public void stop() {
         //Logout
         try {
-            HttpPost post = new HttpPost(this.url + ELVIS_BASE_PATH + "/logout");
-            this.httpClient.execute(post);
-            this.httpClient.close();
-        } catch (Exception e) {
+            get("/logout");
+        } catch (IOException e) {
             logger.error("Could not logout from the ELVIS API !", e.getMessage());
         }
     }
@@ -137,15 +131,9 @@ public class ElvisDataSource extends FilesDataSource {
         this.externalContentStoreProvider = externalContentStoreProvider;
     }
 
-    private HttpResponse get(String endOfUri) throws IOException {
-        HttpGet get = new HttpGet(this.url + ELVIS_BASE_PATH + endOfUri);
-        get.addHeader("Authorization", "Basic " + getAuthentication());
+    private CloseableHttpResponse get(String endOfUri) throws IOException {
+        HttpGet get = new HttpGet(this.url + "/services" + endOfUri);
         get.setHeader("Accept", "Application/Json");
-        get.setHeader("Content-Type", "Application/Json;charset=utf-8");
-        return this.httpClient.execute(get, context);
-    }
-
-    private String getAuthentication() {
-        return Base64.encodeBase64String((this.userName + ":" + this.password).getBytes());
+        return this.httpClient.execute(get, this.context);
     }
 }
